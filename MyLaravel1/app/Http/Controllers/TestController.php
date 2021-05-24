0ee6bca5b507af307fe678ef0cb142534bc697dd
Simone <marzolla.istruzione@gmail.com>
2021-05-24 11:00:24 +0200

    Esercizio concluso

--- MyLaravel1/app/Http/Controllers/TestController.php
+++ MyLaravel1/app/Http/Controllers/TestController.php
@@ -4,7 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+class movie {
+    public $title;
+    public $desc;
+
+    public function __construct($title, $desc){
+    
+        $this -> title = $title;
+        $this -> desc = $desc;
+    
+    }
+    
+    public function getString() {
+    
+        return 
+        'Titolo: ' . $this -> value . ' Descrizione: ' . $this -> desc;
+    
+    }
+}
 class TestController extends Controller
 {
-    //
+    public function home()
+    {
+        $films = new movie('Matrix', 'Un film a bit');
+        $films2 = new movie ('L incredibile Hulk' , 'AAAA');
+        dd($films, $films2);
+        return view('home');
+    }
 }
